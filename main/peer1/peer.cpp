4ef8c348a8635a20a7b541dcf75ebce3b75bb5cc
jay-krishna <jayk674@gmail.com>
2019-10-02 16:28:19 +0530

    Clean up

--- main/peer1/peer.cpp
+++ main/peer1/peer.cpp
@@ -17,43 +17,43 @@
 #include <vector>
 #include <set>
 
-#include "peerhelper.h"
-
 using namespace std;
 
-// bool UserLogin(string username,string password){
-// 	struct sockaddr_in remote_server;
-// 	int sock;
-// 	char output[MAX_SIZE];
+#define MAX_SIZE 1024
 
-// 	if((sock=socket(AF_INET,SOCK_STREAM,0))==-1){
-// 		perror("socket");
-// 		exit(-1);
-// 	}
+bool UserLogin(string username,string password){
+	struct sockaddr_in remote_server;
+	int sock;
+	char output[MAX_SIZE];
 
-// 	remote_server.sin_family=AF_INET;
-// 	remote_server.sin_port=htons(10000);
-// 	remote_server.sin_addr.s_addr=inet_addr("127.0.0.1");
-// 	bzero(&remote_server.sin_zero,8);
+	if((sock=socket(AF_INET,SOCK_STREAM,0))==-1){
+		perror("socket");
+		exit(-1);
+	}
 
-// 	if((connect(sock,(struct sockaddr*)&remote_server,sizeof(struct sockaddr_in)))==-1){
-// 		perror("connect");
-// 		exit(-1);
-// 	}
+	remote_server.sin_family=AF_INET;
+	remote_server.sin_port=htons(10000);
+	remote_server.sin_addr.s_addr=inet_addr("127.0.0.1");
+	bzero(&remote_server.sin_zero,8);
 
-// 	string data="login "+username+" "+password;
-// 	send(sock,data.c_str(),data.size(),0);
-// 	int len=recv(sock,output,MAX_SIZE,0);
-// 	output[len]='\0';
-// 	cout<<output<<endl;
+	if((connect(sock,(struct sockaddr*)&remote_server,sizeof(struct sockaddr_in)))==-1){
+		perror("connect");
+		exit(-1);
+	}
 
-// 	if(output[0]=='0')
-// 		return false;
-// 	else
-// 		return true;
+	string data="login "+username+" "+password;
+	send(sock,data.c_str(),data.size(),0);
+	int len=recv(sock,output,MAX_SIZE,0);
+	output[len]='\0';
+	cout<<output<<endl;
 
-// 	return true;
-// }
+	if(output[0]=='0')
+		return false;
+	else
+		return true;
+
+	return true;
+}
 
 int main(int argc, char** argv){
 
